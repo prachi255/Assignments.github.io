8ae22f54080ee590bd9c22c9a2220b79edb8482b
prachi255 <91514939+prachi255@users.noreply.github.com>
2022-08-06 08:42:17 +0530

    Add files via upload

---
+++ theme-changer/script.js
@@ -0,0 +1,24 @@
+function setblack() {
+    document.getElementById("contentid").style.backgroundColor = "black";
+    document.getElementById("textid").style.borderColor = "white";
+    document.getElementById("textid").style.backgroundColor = "black";
+    document.getElementById("textid").style.color = "white";
+  }
+  function setblue() {
+      document.getElementById("contentid").style.backgroundColor = "blue";
+      document.getElementById("textid").style.borderColor = "white";
+      document.getElementById("textid").style.backgroundColor = "blue";
+      document.getElementById("textid").style.color = "white";
+  }
+  function setgreen() {
+      document.getElementById("contentid").style.backgroundColor = "green";
+      document.getElementById("textid").style.borderColor = "black";
+      document.getElementById("textid").style.backgroundColor = "green";
+      document.getElementById("textid").style.color = "black";
+  }
+  function setyellow() {
+      document.getElementById("contentid").style.backgroundColor = "yellow";
+      document.getElementById("textid").style.borderColor = "black";
+      document.getElementById("textid").style.backgroundColor = "yellow";
+      document.getElementById("textid").style.color = "black";
+  }
